431910522703206705b4d2b5fc8b8ea2eddc7fe0
Will Wise <william.wise@siteminder.com>
2018-07-27 14:48:30 +1000

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class HelloWorld extends Vue {
+  msg = 'Hello world'
+}
+</script>
+
+<style scoped>
+  /* ... */
+</style>
